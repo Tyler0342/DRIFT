14e249e4e1bddc0ba6c6bbb13562ab44770ce243
tylermaggio <tylermaggio@protonmail.com>
2022-07-03 17:51:23 -0600

    Finish BandSocials component

--- src/components/bandsocials/BandSocials.jsx
+++ src/components/bandsocials/BandSocials.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { facebook, twitter, instagram, snapchat, youtube } from './imports';
+import './bandsocials.css';
 
-const BandSocials = () => {
-  return (
-    <div>BandSocials</div>
-  )
-}
+const BandSocials = () => (
+  <div className="drift__bandsocials section__padding">
+    <div>
+      <img src={facebook} />
+    </div>
+    <div>
+      <img src={twitter} />
+    </div>
+    <div>
+      <img src={instagram} />
+    </div>
+    <div>
+      <img src={snapchat} />
+    </div>
+    <div>
+      <img src={youtube} />
+    </div>
+        <div className="drift__socials-btn">
+          <h1>Join our newsletter for exclusive access</h1>
+      <button type="button">Join</button>
+    </div>
+  </div>
+);
 
-export default BandSocials
+export default BandSocials;
